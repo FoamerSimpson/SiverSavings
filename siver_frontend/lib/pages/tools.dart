d8b46f62dc208c92491784ec3d3523bbe134562b
foamer simpson <falconeer9964@gmail.com>
2024-06-26 12:52:37 -0500

    update

--- siver_frontend/lib/pages/tools.dart
+++ siver_frontend/lib/pages/tools.dart
@@ -16,23 +16,43 @@ class _toolsState extends State<tools> {
       ),
       body: Center(
         child: Column(children: [
-          TextButton(
-            onPressed: () {},
-            child: Text('Investment Calculator'),
-            style: ButtonStyle(
-              
-              padding: WidgetStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 15.0, horizontal: 120.0 )),
-              shadowColor: WidgetStateProperty.all<Color>(Color.fromARGB(255, 173, 96, 152)),
-              backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 137, 211, 141)),
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
-                side: BorderSide(color: Colors.red)
-                )
+          Container(
+            width: 350,
+            child: TextButton(
+              onPressed: () {},
+              child: Text('Investment Calculator'),
+              style: ButtonStyle(
+                //padding: WidgetStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 15.0, horizontal: 120.0 )),
+                shadowColor: WidgetStateProperty.all<Color>(Color.fromARGB(255, 173, 96, 152)),
+                backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 137, 211, 141)),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                  side: BorderSide(color: Colors.red)
+                  )
+                ),
               ),
             ),
           ),
-          Text("hello"),
+          Container(
+            width: 350,
+            child: TextButton(
+              onPressed: () {},
+              child: Text('Morgage Calculator'),
+              style: ButtonStyle(
+                
+                //padding: WidgetStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 15.0, horizontal: 120.0 )),
+                shadowColor: WidgetStateProperty.all<Color>(Color.fromARGB(255, 173, 96, 152)),
+                backgroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 137, 211, 141)),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                  side: BorderSide(color: Colors.red)
+                  )
+                ),
+              ),
+            ),
+          ),
         ],),
       ),
     );
