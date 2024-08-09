548bbe7b435a21a110f0eaa2f700f15f87b9794b
foamer simpson <falconeer9964@gmail.com>
2024-08-09 13:22:43 -0500

    polished up stocks tab

--- siver_frontend/lib/pages/objects/stockobj.dart
+++ siver_frontend/lib/pages/objects/stockobj.dart
@@ -1,7 +1,7 @@
 class Stock {
 
   final String ticker;
-  final String value;
+  final double value;
 
   Stock({required this.ticker, required this.value});
 
--- siver_frontend/lib/pages/stocks.dart
+++ siver_frontend/lib/pages/stocks.dart
@@ -12,36 +12,50 @@ class Stocks extends StatefulWidget {
 
 class _StocksState extends State<Stocks> {
   List<Stock> Stocklist = [
-    Stock(ticker: "APPL", value: "102"),
-    Stock(ticker: "FORD", value: "122"),
-    Stock(ticker: "TSLA", value: "142")
+    
   ];
 
   final TextEditingController tickerController = TextEditingController();
 
   Future<void> fetchStockPrice(String ticker) async {
-    final String apiUrl = 'http://10.0.2.2:5000/get_stock_price/$ticker';
+  final String apiUrl = 'http://10.0.2.2:5000/get_stock_price/$ticker';
 
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      if (data['price'] != null) {
         final String stockPrice = data['price'].toString();
-        
-        
+
         setState(() {
-          Stocklist.add(Stock(ticker: ticker, value: stockPrice));
+          double x = double.parse(stockPrice);
+          Stocklist.add(Stock(ticker: ticker.toUpperCase(), value: (x * 100).round() / 100));
         });
 
         tickerController.clear();
       } else {
-        
-        print("Failed to fetch stock price: ${response.statusCode}");
+        print("$ticker Does not seem to be a stock, try another.");
+        _showErrorMessage("$ticker Does not seem to be a stock, try another.");
       }
-    } catch (e) {
-      print("Error fetching stock price: $e");
+    } else {
+      print("Failed to fetch stock price: ${response.statusCode}");
+      _showErrorMessage("Failed to fetch stock price: ${response.statusCode}");
     }
+  } catch (e) {
+    print("Error fetching stock price: $e");
+    _showErrorMessage("Error fetching stock price: $e");
   }
+}
+
+void _showErrorMessage(String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    ),
+  );
+}
 
   Widget stockTemplate(Stock stock) {
     return Card(
